bc943adddb312904b021c1155b4dd27ecad81c82
IS31TALDEA <ik012982i1@H012982.NET>
2018-01-19 11:42:14 +0100

    collision reached

--- js/apple.js
+++ js/apple.js
@@ -12,22 +12,81 @@ var canvas = document.getElementById("modelZone"); //get the canvas dom object
 var ctx = canvas.getContext("2d"); //get the context
 
 var redraw = function(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height); //clear canvas
-    ctx.beginPath();  //draw the object c
-    ctx.closePath();
-    ctx.fill();
-      
-    requestAnimationFrame(redraw);
-  }
+  ctx.clearRect(0, 0, canvas.width, canvas.height); //clear canvas
+  ctx.beginPath();  //draw the object c
+  ctx.closePath();
+  ctx.fill();
+    
+  requestAnimationFrame(redraw);
+};
 
 function move(){
-  var f = Math.random() // this returns a float between 0.0 and 1.0
-  a = f * canvas.width;
-  b = f * canvas.height;
-}
-redraw();
-
-setInterval(move, 1000);
+var w = Math.random()
+var h = Math.random() // this returns a float between 0.0 and 1.0
+a = w * canvas.width;
+b = h * canvas.height;
+};
+redraw(); 
 
 document.getElementById("box2").style.left = a + "px"
 document.getElementById("box2").style.top = b + "px"
+
+document.getElementById('box').style.left = "50px";
+document.getElementById("box").style.top = "50px";
+
+var x = 50;
+var y = 50;
+var dx = 20;
+var dy = 20;
+
+
+    document.addEventListener ('keydown', function(event) {
+        console.log(event.keyCode);
+        
+
+        if (event.keyCode == 39) {
+            x = x + dx;
+        };
+        if (event.keyCode == 37) {
+            x = x - dx;
+        };
+        if (event.keyCode == 40) {
+            y = y + dy;
+        };
+        if (event.keyCode == 38) {
+            y = y - dy;
+        };
+        document.getElementById("box").style.left = x + "px"
+        document.getElementById("box").style.top = y + "px"
+        
+        var hy = {posx: x, posy: y, width: 40, height: 40}
+        var wx = {posa: a, posb: b, width: 40, height: 40}
+        
+        if (hy.posx < (wx.posa + wx.width)+20 &&
+         (hy.posx + hy.width)+ 20 > wx.posa &&
+         hy.posy < (wx.posb + wx.height)+20 &&
+         (hy.height + hy.posy)+20 > wx.posb) {
+          // collision detected!
+          console.log("collision")
+          redraw();
+          
+        };
+        if (x < 10) {
+            document.getElementById("box").style.visibility= "hidden";
+            alert("you lost");
+        }
+        else if (x > 1370) {
+            document.getElementById("box").style.visibility= "hidden";
+            alert("you lost");
+        }
+        else if (y < 10) {
+            document.getElementById("box").style.visibility= "hidden";
+            alert("you lost");
+        }
+        else if (y > 870) {
+            document.getElementById("box").style.visibility= "hidden";
+            alert("you lost");
+        }
+    });
+
+    
